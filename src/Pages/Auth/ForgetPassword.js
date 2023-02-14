44f33a9c42a400a5db75bb9142b7044900a68ff8
mai-sami <mai.jaber@hotmail.com>
2023-02-14 11:56:01 +0200

    Auth page

--- src/Pages/Auth/ForgetPassword.js
+++ src/Pages/Auth/ForgetPassword.js
@@ -1,10 +1,46 @@
+import { yupResolver } from '@hookform/resolvers/yup';
 import React from 'react'
+import { useForm } from 'react-hook-form';
+import { Link } from 'react-router-dom';
+import Button from '../../Components/Buttons/Button';
+import ErrorMessage from '../../Components/ErrorMessages/ErrorMessage';
+import InputForms from '../../Components/INPUTandLabels/InputForms';
+import Title from '../../Components/Titles/Title'
+import { Form } from '../../Style/elments'
+import { Flex } from '../../Style/layout';
+import { loginSchema } from './Validation';
 
 function ForgetPassword() {
+    const { register, handleSubmit, getValues, formState: { errors } } = useForm({
+        resolver: yupResolver(loginSchema)
+    });
+    const onSubmit = () => {
+        const email = getValues("email")
+        console.log("email is", email)
+    }
     return (
-        <>
-        ForgetPassword
-        </>
+        <Form onSubmit={handleSubmit(onSubmit)}>
+            <Title name={"FORGOT PASSWORD"} />
+            <InputForms
+                text={"Email ID"}
+                placeholder={"Enter Email ID"}
+                type={"email"}
+                register={register}
+                border={errors?.email ? "red" : "#bababaa0"}
+                name="email"
+            />
+            <ErrorMessage>
+                {errors.email && <ErrorMessage>{errors.email.message}</ErrorMessage>}
+            </ErrorMessage>
+            <Flex>
+                <Link to={"/verification"}>
+                <Button
+                    name={"SUBMIT"}
+                    margin={"2rem"}
+                    />
+                    </Link>
+            </Flex>
+        </Form>
     )
 }
 
--- src/Pages/Auth/Verification.js
+++ src/Pages/Auth/Verification.js
@@ -1,9 +1,34 @@
+import { yupResolver } from '@hookform/resolvers/yup';
 import React from 'react'
+import { Link } from 'react-router-dom';
+import Anckors from '../../Components/AnckorComponent/Anckors';
+import Button from '../../Components/Buttons/Button';
+import Title from '../../Components/Titles/Title'
+import { Form } from '../../Style/elments'
+import { Flex } from '../../Style/layout';
 
 function Verification() {
+
     return (
-        <></>
+        <Form >
+            <Title name={"Verification"} />
+            
+            <Anckors
+                 name={"www.loremipsum.com"}
+                color={"#FF0000"}
+                href={"/register"} />
+            <Flex>
+                <Link to={"/"}>
+                    <Button
+                        name={"SUBMIT"}
+                        margin={"2rem"}
+                    />
+                </Link>
+            </Flex>
+        </Form>
     )
 }
+
+
 
 export default Verification
